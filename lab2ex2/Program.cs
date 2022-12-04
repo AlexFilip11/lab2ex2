9afdf105abe9b173632da68d6bedfa48eb8c8e4c
Alex Filip <alexfilippaul@gmail.com>
2022-12-04 23:05:41 +0200

    Add project files.

---
+++ lab2ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab2ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrieti un program care va afisa factorialul unui numar n, n fiind citit de la tastatura.
+            Console.WriteLine("Introduceti un numar: ");
+            int n = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            for(int i=1; i<=n; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine(n + " factorial este " + factorial);
+        }
+    }
+}
